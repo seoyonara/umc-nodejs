83faa35b8e149e3d14c41a9c5d8a374029733c56
seoyonara <syp614@ewhain.net>
2025-05-28 01:26:46 +0900

    ✨ 9주차 실습

--- src/index.js
+++ src/index.js
@@ -14,10 +14,25 @@ import {
 import swaggerAutogen from 'swagger-autogen';
 import swaggerUiExpress from 'swagger-ui-express';
 
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import session from 'express-session';
+import passport from 'passport';
+import { googleStrategy } from './auth.config.js';
+import { prisma } from './db.config.js';
+
 dotenv.config();
+
+passport.use(googleStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors()); // cors 방식 허용
+app.use(express.static('public')); // 정적 파일 접근
+app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 app.use((req, res, next) => {
   res.success = (success) => {
@@ -35,12 +50,27 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors()); // cors 방식 허용
-app.use(express.static('public')); // 정적 파일 접근
-app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
-app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
+  console.log(req.user);
   res.send('Hello World!');
 });
 
@@ -53,6 +83,16 @@ app.get('/reviews/users/:userId', handleUserReviewList);
 app.get('/missions/stores/:storeId', handleStoreMissionList);
 app.get('/missions/users/:userId', handleUserMissionList);
 app.patch('/missions/:userMissionId/clear', handleMissionClear);
+
+app.get('/oauth2/login/google', passport.authenticate('google'));
+app.get(
+  '/oauth2/callback/google',
+  passport.authenticate('google', {
+    failureRedirect: '/oauth2/login/google',
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect('/')
+);
 
 app.use((err, req, res, next) => {
   if (res.headersSent) {
