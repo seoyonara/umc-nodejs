e26aec5fe1c380e74f73c9c0e68e8745cbb074cc
seoyonara <syp614@ewhain.net>
2025-05-28 02:28:27 +0900

    ✨ 9주차 미션

--- src/auth.config.js
+++ src/auth.config.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as KakaoStrategy } from 'passport-kakao';
 import { prisma } from './db.config.js';
 
 dotenv.config();
@@ -12,7 +13,7 @@ export const googleStrategy = new GoogleStrategy(
     scope: ['email', 'profile'],
     state: true,
   },
-  (accessToken, refreshToken, profile, cb) => {
+  async (accessToken, refreshToken, profile, cb) => {
     return googleVerify(profile)
       .then((user) => cb(null, user))
       .catch((err) => cb(err));
@@ -21,6 +22,44 @@ export const googleStrategy = new GoogleStrategy(
 
 const googleVerify = async (profile) => {
   const email = profile.emails?.[0]?.value;
+  if (!email) {
+    throw new Error(`profile.email was not found: ${profile}`);
+  }
+
+  const user = await prisma.user.findFirst({ where: { email } });
+  if (user !== null) {
+    return { id: user.id, email: user.email, name: user.name };
+  }
+
+  const created = await prisma.user.create({
+    data: {
+      email,
+      name: profile.displayName,
+      gender: 'F',
+      birth: new Date(1970, 0, 1),
+      address1: '추후 수정',
+      address2: '추후 수정',
+      phoneNumber: '추후 수정',
+    },
+  });
+
+  return { id: created.id, email: created.email, name: created.name };
+};
+
+export const kakaoStrategy = new KakaoStrategy(
+  {
+    clientID: process.env.KAKAO_REST_API_KEY,
+    callbackURL: 'http://localhost:3000/oauth2/callback/kakao',
+  },
+  async (accessToken, refreshToken, profile, cb) => {
+    return kakaoVerify(profile)
+      .then((user) => cb(null, user))
+      .catch((err) => cb(err));
+  }
+);
+
+const kakaoVerify = async (profile) => {
+  const email = profile._json.kakao_account?.email;
   if (!email) {
     throw new Error(`profile.email was not found: ${profile}`);
   }
--- src/index.js
+++ src/index.js
@@ -17,12 +17,13 @@ import swaggerUiExpress from 'swagger-ui-express';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import session from 'express-session';
 import passport from 'passport';
-import { googleStrategy } from './auth.config.js';
+import { googleStrategy, kakaoStrategy } from './auth.config.js';
 import { prisma } from './db.config.js';
 
 dotenv.config();
 
 passport.use(googleStrategy);
+passport.use(kakaoStrategy);
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
@@ -89,6 +90,16 @@ app.get(
   '/oauth2/callback/google',
   passport.authenticate('google', {
     failureRedirect: '/oauth2/login/google',
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect('/')
+);
+
+app.get('/oauth2/login/kakao', passport.authenticate('kakao'));
+app.get(
+  '/oauth2/callback/kakao',
+  passport.authenticate('kakao', {
+    failureRedirect: '/oauth2/login/kakao',
     failureMessage: true,
   }),
   (req, res) => res.redirect('/')
